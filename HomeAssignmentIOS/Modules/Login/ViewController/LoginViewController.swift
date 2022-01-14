ce7f5e050769edb1d919b8392a387838ed4be6c3
Saurabh Rana <saurabh.a.rana@accenture.com>
2022-01-14 12:41:46 +0530

    Created Project Structure and pushed all files

---
+++ HomeAssignmentIOS/Modules/Login/ViewController/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  HomeAssignmentIOS
+//
+//  Created by saurabh.a.rana on 13/01/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
